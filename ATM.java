cfcf35e514120d8b0f2f54eeaf39169a5def6565
Nataliia21885 <102697132+Nataliia21885@users.noreply.github.com>
2022-04-25 16:59:13 +0300

    Create ATM.java

---
+++ ATM.java
@@ -0,0 +1,39 @@
+public class ATM {
+    public int countBanknotes(int sum){
+        int counter = 0;
+        
+          while(sum > 0) {
+              if(sum >= 500) {
+                sum = sum - 500;
+                
+              } else if(sum >= 200) {
+                sum = sum - 200;
+                
+              }else if(sum >= 100) {
+                sum = sum - 100;
+                
+              }else if(sum >= 50) {
+                sum = sum - 50;
+                
+              }else if(sum >= 20) {
+                sum = sum - 20;
+                
+              }else if(sum >= 10) {
+                sum = sum - 10;
+                
+              }else if(sum >= 5) {
+                sum = sum - 5;
+                
+              }else if(sum >= 2) {
+                sum = sum - 2;
+                
+              }else if(sum >= 1) {
+                sum = sum - 1;
+              }
+            counter++;
+           }
+    return counter;
+    
+    }
+}
+
